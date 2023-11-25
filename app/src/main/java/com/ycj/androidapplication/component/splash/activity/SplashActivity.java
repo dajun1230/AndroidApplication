87c43d06212b5a5d1aafbbfb845cbf503f48fac1
小杨 <1123395547@qq.com>
2023-11-25 09:59:00 +0800

    git commit -m "feat: 修改顶部状态栏"

--- app/src/main/java/com/ycj/androidapplication/component/splash/activity/SplashActivity.java
+++ app/src/main/java/com/ycj/androidapplication/component/splash/activity/SplashActivity.java
@@ -3,8 +3,11 @@ package com.ycj.androidapplication.component.splash.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.qmuiteam.qmui.util.QMUIStatusBarHelper;
 import com.ycj.androidapplication.R;
+import com.ycj.androidapplication.util.SuperDateUtil;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,5 +15,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //设置沉浸式状态栏
+        QMUIStatusBarHelper.translucent(this);
+
+        //设置状态栏文字颜色
+        QMUIStatusBarHelper.setStatusBarLightMode(this);
+
+        //设置本部年份
+        int year = SuperDateUtil.currentYear();
+
+        TextView copyrightView = findViewById(R.id.copyright);
+        copyrightView.setText(getResources().getString(R.string.copyright, year));
     }
 }
