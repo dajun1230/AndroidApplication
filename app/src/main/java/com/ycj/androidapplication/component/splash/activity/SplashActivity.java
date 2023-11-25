cb1d607f5f52f993bcfd1e29333a9d4c1d1fd21a
小杨 <1123395547@qq.com>
2023-11-25 10:15:20 +0800

    git commit -m "feat: 增加全局Activity"

--- app/src/main/java/com/ycj/androidapplication/component/splash/activity/SplashActivity.java
+++ app/src/main/java/com/ycj/androidapplication/component/splash/activity/SplashActivity.java
@@ -3,13 +3,18 @@ package com.ycj.androidapplication.component.splash.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.qmuiteam.qmui.util.QMUIStatusBarHelper;
 import com.ycj.androidapplication.R;
+import com.ycj.androidapplication.activity.BaseActivity;
+import com.ycj.androidapplication.util.SuperDarkUtil;
 import com.ycj.androidapplication.util.SuperDateUtil;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends BaseActivity {
+
+    private TextView copyrightView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +25,27 @@ public class SplashActivity extends AppCompatActivity {
         QMUIStatusBarHelper.translucent(this);
 
         //设置状态栏文字颜色
-        QMUIStatusBarHelper.setStatusBarLightMode(this);
+        if (SuperDarkUtil.isDark(this)) {
+            //状态栏文字白色
+            QMUIStatusBarHelper.setStatusBarDarkMode(this);
+        } else {
+            //状态栏黑色
+            QMUIStatusBarHelper.setStatusBarLightMode(this);
+        }
+    }
 
-        //设置本部年份
+    @Override
+    protected void initView() {
+        super.initView();
+        //快捷生成实例变量：command+option+f  或者右键>refactor>Introduce Field...
+        copyrightView = findViewById(R.id.copyright);
+    }
+
+    @Override
+    protected void initDatum() {
+        super.initDatum();
+        //设置年份
         int year = SuperDateUtil.currentYear();
-
-        TextView copyrightView = findViewById(R.id.copyright);
         copyrightView.setText(getResources().getString(R.string.copyright, year));
     }
 }
